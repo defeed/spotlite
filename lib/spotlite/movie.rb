86bd876faf217553e1f4c0cfed3d44ce9b86c9ee
Artem Pakk <tema@tema.ee>
2013-01-09 23:47:08 +0200

    Title, year, rating, votes methods for movie

---
+++ lib/spotlite/movie.rb
@@ -0,0 +1,51 @@
+module Spotlite
+
+  class Movie
+    attr_accessor :imdb_id, :title, :url
+    
+    def initialize(imdb_id, title = nil, url = nil)
+      @imdb_id = imdb_id
+      @title   = title
+      @url     = "http://www.imdb.com/title/tt#{imdb_id}/"
+    end
+    
+    def title
+      @title ||= combined.css("#tn15title h1").children.first.text.strip
+    end
+    
+    def year
+      combined.search('a[href^="/year/"]').text.to_i rescue nil
+    end
+    
+    def rating
+      combined.css("div.starbar-meta b").children.first.text.split("/").first.to_f rescue nil
+    end
+    
+    def votes
+      combined.css("div.starbar-meta a.tn15more").text.gsub(/[^\d+]/, "").to_i rescue nil
+    end
+    
+    private
+    
+    def combined
+      Nokogiri::HTML(open_page(@imdb_id, "combined"))
+    end
+    
+    def release_info
+      Nokogiri::HTML(open_page(@imdb_id, "releaseinfo"))
+    end
+    
+    def full_credits
+      Nokogiri::HTML(open_page(@imdb_id, "fullcredits"))
+    end
+    
+    def plot_keywords
+      Nokogiri::HTML(open_page(@imdb_id, "keywords"))
+    end
+    
+    def open_page(imdb_id, page)
+      open("http://akas.imdb.com/title/tt#{imdb_id}/#{page}")
+    end
+  end
+
+end
