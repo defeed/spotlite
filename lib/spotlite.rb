6de38da3f0badf9e93dc0b64bb08e26973ccb26e
Artem Pakk <tema@tema.ee>
2013-01-09 23:48:51 +0200

    Fixed require typo

--- lib/spotlite.rb
+++ lib/spotlite.rb
@@ -1,7 +1,7 @@
 $:.unshift(File.dirname(__FILE__)) unless
   $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
 
-require "ruby gems"
+require "rubygems"
 require "open-uri"
 require "nokogiri"
 
