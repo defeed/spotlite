813d460b2489c760c0be94ca97618bad5077397f
Artem Pakk <apakk@me.com>
2013-04-20 17:22:30 +0300

    Updated fixtures. Fixed issue with original non-english titles are now in quotes

--- lib/spotlite/movie.rb
+++ lib/spotlite/movie.rb
@@ -27,7 +27,7 @@ module Spotlite
     
     # Returns original non-english title as a string
     def original_title
-      details.at("h1.header span.title-extra[itemprop='name']").children.first.text.strip rescue nil
+      details.at("h1.header span.title-extra[itemprop='name']").children.first.text.gsub('"', "").strip rescue nil
     end
     
     # Returns year of original release as an integer
