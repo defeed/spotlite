cb089c80c3f0ed70bbfcea49e399056a0f5a142d
Artem Pakk <apakk@me.com>
2013-04-30 22:20:51 +0300

    Fixed issue with [movie] titles not necessarily being in the first table on search results page

--- lib/spotlite/search.rb
+++ lib/spotlite/search.rb
@@ -17,17 +17,22 @@ module Spotlite
     end
     
     def parse_movies
-      page.at("table.findList").css("td.result_text").reject do |node|
-        # search results will only include movies
-        SKIP.any? { |skipped| node.text.include? skipped }
-      end.map do |node|
-        imdb_id = node.at("a[href^='/title/tt']")['href'].parse_imdb_id
-        title   = node.at("a").text.strip
-        year    = node.children.last.text.parse_year
+      table = page.at("a[name='tt']").parent.parent.at("table.findList") rescue nil
+      if !table.nil?
+        table.css("td.result_text").reject do |node|
+          # search results will only include movies
+          SKIP.any? { |skipped| node.text.include? skipped }
+        end.map do |node|
+          imdb_id = node.at("a[href^='/title/tt']")['href'].parse_imdb_id
+          title   = node.at("a").text.strip
+          year    = node.children.last.text.parse_year
         
-        [imdb_id, title, year]
-      end.map do |values|
-        Spotlite::Movie.new(*values)
+          [imdb_id, title, year]
+        end.map do |values|
+          Spotlite::Movie.new(*values)
+        end
+      else
+        return []
       end
     end
     
