74b531c3a9b25f1761cf5f2b77099a88f0c46c65
Artem Pakk <apakk@me.com>
2013-05-01 22:24:57 +0300

    Some comments to string_extensions

--- lib/spotlite/string_extensions.rb
+++ lib/spotlite/string_extensions.rb
@@ -1,7 +1,10 @@
 class String
   require 'date' # for Ruby 1.9.2
   
-  def parse_date # :nodoc:
+  # Parses date from a string like '20 Jan 2013', 'Mar 2013', or '2013'. 
+  # Will return 01-Mar-2013 in case of 'Mar 2013'. 
+  # Will return 01-Jan-2013 in case of '2013' 
+  def parse_date
     begin
       length > 4 ? Date.parse(self) : Date.new(self.to_i)
     rescue ArgumentError
@@ -14,15 +17,18 @@ class String
     year > 0 ? year : nil
   end
   
+  # Strips 4 digits in braces and a single space before from a string like 'Movie Title (2013)'
   def strip_year
     gsub(/\s\((\d{4})\)/, "")
   end
 
-  def clean_href # :nodoc:
+  # Cleans 'href' param of an <a> tag
+  def clean_href
     gsub(/\?ref.+/, "").gsub("/country/", "").gsub("/language/", "")
   end
     
-  def parse_imdb_id # :nodoc:
+  # Parses 7-digit IMDb ID, usually from a URL
+  def parse_imdb_id
     self[/\d{7}/] unless self.nil?
   end
 
